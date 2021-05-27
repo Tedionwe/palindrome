72d5667d3aba5b1119ec805b36992f9be16e086a
MAC <user@MACs-MacBook-Air.local>
2021-05-27 11:59:21 +0100

    add

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * A palindrome is a word or sentence that's spelt the same way both forward and backward,
+ * ignoring punctuation, case and spacing
+ *
+ */
+
+
 function palindrome(str) {
     let polishedString = str.replace(/\W+|_/g, "").toLowerCase();
     let reversedString = polishedString.split("").reverse().join("");
